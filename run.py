7e410f6264cab3ab6bc8864da2ef6e56cf2e93d4
winniekariuki <winniekariuki07@gmail.com>
2018-11-02 09:40:06 +0300

    [bug #161671568] modify files

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 from app import create_app
 from app.api.v2.models import *
+
 data = {"username":"Winnie","email":"winniekariuki07@gmail.com","password":"winnie07@","role":"Admin"}
 admin = User(data)
 admin.save_admin() 
